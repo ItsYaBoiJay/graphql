1035bc4fd6a2c97eb24798bec99ab9e1e89025ae
crippe111 <crippe-b@hotmail.com>
2023-05-02 19:25:56 +0300

    react is fucking wierd

--- my-react-app/src/LoginPage.js
+++ my-react-app/src/LoginPage.js
@@ -184,45 +184,128 @@ async function getTheRest() {
 // </g>
 // </svg>`
 console.log("dajs",data.data.xp)
-el.appendChild(createLineChart(data.data.xp))
+el.append(createLineChart(data.data.xp, data.data.xpTotal.aggregate.sum.amount, "all"), createLineChart(data.data.xpJS, data.data.xpJsTotal.aggregate.sum.amount, "js"), createLineChart(data.data.xpGo, data.data.xpGoTotal.aggregate.sum.amount, "go"))
+let all = document.createElement("button")
+let go = document.createElement("button")
+let js = document.createElement("button")
+
+
+all.innerHTML += "Projects"
+all.setAttribute("onclick", `showGraph("all")`)
+go.innerHTML += "Go piscine"
+go.setAttribute("onclick", `showGraph("go")`)
+js.innerHTML += "Js piscine"
+js.setAttribute("onclick", `showGraph("js")`)
+el.append(all,go,js)
 document.body.appendChild(el)
+//showGraph("all")
+// return (<button onClick={showGraph("all")}>
+//   Click me
+// </button>)
+// document.getElementsByClassName("all")[0].onclick = showGraph("all")
+// document.getElementsByClassName("go")[0].onclick = showGraph("go")
+// document.getElementsByClassName("js")[0].onclick = showGraph("js")
 
 }
+function showGraph(wtf, graph = "all") {
+  console.log("graph chosen", graph)
+  document.getElementsByClassName("all")[0].style.display = "none"
+  document.getElementsByClassName("go")[0].style.display = "none"
+  document.getElementsByClassName("js")[0].style.display = "none"
+  document.getElementsByClassName(graph)[0].style.display = "block"
+  console.log("something happened")
+}
 
-function createLineChart(dataArray) {
-  let yearMin = [dataArray[0].createdAt]
-  let yearMax = [dataArray[dataArray.length-1].amount]
+function createLineChart(dataArray, xpTotal, name) {
+  console.log("should be a number", xpTotal)
+  let yearMin = new Date(dataArray[0].createdAt)
+  let yearMax = new Date(dataArray[dataArray.length-1].createdAt)
+  let intervalLength = (yearMax.getTime() - yearMin.getTime()) / 4
   let xpMin = 0
-  let xpMax = dataArray[dataArray.length-1].amount
+  console.log("xptotal", xpTotal)
+  let xpMax = xpTotal /* dataArray[dataArray.length-1].amount */
   let el = document.createElement("svg")
-  el.innerHTML += `<svg version="1.2" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" class="graph" aria-labelledby="title" role="img">
-  <title id="title">A line chart showing some information</title>
+  el.innerHTML += `<svg version="1.2" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" class="graph ${name}" aria-labelledby="title" role="img">
 <g class="grid x-grid" id="xGrid">
   <line x1="90" x2="90" y1="5" y2="371"></line>
 </g>
 <g class="grid y-grid" id="yGrid">
   <line x1="90" x2="705" y1="370" y2="370"></line>
 </g>`
+// console.log("this better work", yearMax.getTime() - yearMin.getTime())
 let year = document.createElement("g")
 let xp = document.createElement("g")
 year.classList.add("labels", "x-labels")
 xp.classList.add("labels","y-labels")
 for (let i = 0; i < 5; i++) {
-  year.innerHTML += `<text y="400" x="${100 + (100 * i)}">${yearMin + ((yearMax / 5) * i)}</text`
-  xp.innerHTML += `<text x=80 y="${15 + (115 * i)}">${xpMin + ((xpMax / 5) * i)}</text>`
-}
+    let temp = new Date(yearMin.getTime() + (intervalLength * i))
+    console.log("date", temp.getDay + "/" + temp.getMonth + 1)
+    year.innerHTML += `<text y="400" x="${130 + (150 * i)}">${temp.getDate() + "/" + (temp.getMonth() + 1) + "-" + temp.getFullYear()}</text`
+    xp.innerHTML += `<text x=80 y="${375 - (90 * i)/* 15 + (90 * i) */}">${xpMin + ((xpMax / 4) * i)}</text>`
+ }
+
 year.innerHTML += `<text x="400" y="440" class="label-title">Time wasted</text>`
-xp.innerHTML += `<text x="50" y="200" class="label-title">Total xp</text>`
-el.firstChild.appendChild(year)
-el.firstChild.appendChild(xp)
-el.innerHTML += `<g class="data" data-setname="Our first data set">
-<circle cx="90" cy="192" data-value="7.2" r="4"></circle>
-<circle cx="240" cy="141" data-value="8.1" r="4"></circle>
-<circle cx="388" cy="179" data-value="7.7" r="4"></circle>
-<circle cx="531" cy="200" data-value="6.8" r="4"></circle>
-<circle cx="677" cy="104" data-value="6.7" r="4"></circle>
-</g>`
+// //xp.innerHTML += `<text x="50" y="200" class="label-title">Total xp</text>`
+el.firstChild.innerHTML += `<g class="labels x-labels">` + year.innerHTML + `</g>`
+el.firstChild.innerHTML += `<g class="labels x-labels">` + xp.innerHTML + `</g>`
+// //el.firstChild.appendChild(xp)
+let points = mapOutData(xpMin, xpMax, yearMin.getTime(), yearMax.getTime(), dataArray)
+// console.log(points)
+
+// el.firstChild.innerHTML += `<g class="labels x-labels">
+//   <text x="100" y="400">2008</text>
+//   <text x="246" y="400">2009</text>
+//   <text x="392" y="400">2010</text>
+//   <text x="538" y="400">2011</text>
+//   <text x="684" y="400">2012</text>
+//   <text x="400" y="440" class="label-title">Time wasted</text>
+// </g>`
+
+el.firstChild.innerHTML += `<g class="first-set points data">` + points.innerHTML + `</g>`
+// el.firstChild.innerHTML += `<g class="data" data-setname="Our first data set">
+// <circle cx="90" cy="192" data-value="7.2" r="4"></circle>
+// <circle cx="240" cy="141" data-value="8.1" r="4"></circle>
+// <circle cx="388" cy="179" data-value="7.7" r="4"></circle>
+// <circle cx="531" cy="200" data-value="6.8" r="4"></circle>
+// <circle cx="677" cy="104" data-value="6.7" r="4"></circle>
+// </g>`
 return el.firstChild
+}
+
+function mapOutData(minY, maxY, minX, maxX, arr) {
+  let el = document.createElement("g")
+  let lineYmin = 5
+  let lineYMax = 371
+  let lineXmin = 90
+  //let lineXMax = 705
+  let lineXlen = 615
+  let lineYlen = 366
+  let totalXp = 0
+  let dtest = new Date(arr[arr.length -1].createdAt).getTime()
+  console.log("testo","A", dtest, minX, maxX, maxX - minX, "B", (dtest - minX) / (maxX - minX))
+  for (let i = 0; i < arr.length; i++) {
+    let xp = arr[i].amount
+    totalXp += xp
+    let yCord = lineYMax - (((lineYlen / 100) * ((totalXp / maxY) * 100)) + lineYmin)
+
+    let date = new Date(arr[i].createdAt).getTime() - minX
+    let xCord = ((lineXlen) / 100) * ((date / (maxX - minX)) * 100) + lineXmin
+    let el2 = document.createElement("g")
+    el2.classList.add("bleh")
+    el2.dataset.hover = arr[i].path
+    el2.innerHTML += `
+    <title>Xp gain:${xp}
+Date:${arr[i].createdAt}
+Project:${arr[i].path}</title>
+    <circle cx="${xCord}" cy="${yCord}" r="4"></circle>`
+    el.appendChild(el2)
+    //temp += ` <circle cx="${xCord}" cy="${yCord}" r="4"> <div class="bleh" data-xp="${xp}" data-hover="${arr[i].path}" data-time="${arr[i].createdAt}"></div></circle> `
+  }
+  console.log("total", totalXp)
+  //el.innerHTML += temp
+
+  el.classList.add("first-set", "points", "data")
+  return el
 }
 
 
@@ -274,6 +357,7 @@ function LoginPage() {
           <p>Audit Ratio: {Math.round(userData.data.user[0].auditRatio * 10) / 10}</p>
           <button onClick={logout}>Logout</button>
           <button onClick={getTheRest}>more shit</button>
+          <button onClick={showGraph}>test</button>
         </div>
       ) : (
         <div>
